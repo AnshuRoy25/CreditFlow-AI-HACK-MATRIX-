12b8f3601ef4ba2afea7dde660f8c1022e022829
AnshuRoy25 <24bph025@nith.ac.in>
2026-02-21 06:41:17 +0530

    deleted unnecessary files

--- Logistic-Regression/example-plots/bar.py
+++
@@ -1,8 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-features = ['income', 'cibil', 'dti_ratio']
-coefficients = [1.2, 1.5, -1]
-
-sns.barplot(y = features, x = coefficients)
-plt.show()
--- Logistic-Regression/example-plots/heat.py
+++
@@ -1,8 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-data = [[3, 4, 5], [0, 6, 1], [4, 3, 7], [2, 8, 9], [3, 5, 1]]
-
-plt.figure(figsize=(8, 6))
-sns.heatmap(data, vmin = 0, vmax = 10, annot=True, cmap='Blues')
-plt.show()
